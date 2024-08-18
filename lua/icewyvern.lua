ab3f9963946e815f4b7d74b1de0a6f761cc9461c
Tyler Calder <calder-ty@protonmail.com>
2024-08-17 22:32:05 -0600

    feat: New Color Pallete

--- lua/icewyvern.lua
+++ lua/icewyvern.lua
@@ -1,5 +1,5 @@
 local api =	vim.api;
-local colors = require("lua.palette");
+local colors = require("palette");
 
 local M = {};
 
@@ -24,7 +24,7 @@ function M.highlight(group, style)
 	local sp  = style.sp  and 'guisp='  .. style.sp  or ''
 	api.nvim_command('hi ' .. group .. ' ' .. gui .. ' ' .. fg .. ' ' .. bg .. ' ' .. sp)
 	if (style.link) then
-		vim.cmd(sting.format('highlight! link %s %s', group, color.link))
+		vim.cmd(string.format('highlight! link %s %s', group, style.link))
 	end
 end
 
@@ -34,75 +34,73 @@ function M.load_colors()
 	local styles = M.stylings
 	local syntax = {
 
-
+		LineNr=			{fg=colors.green1,											};
+		CursorLineNr=	{fg=colors.green1,							gui=styles.bold};
+		Visual=			{fg=colors.none,			bg=colors.white2, gui=colors.none};
+		WinSeparator=	{fg=colors.blue5,			bg=colors.none,		gui=colors.none};
+		SignColumn=		{																};
+		ColorColumn=	{fg=colors.none,			bg=colors.black2,	gui=colors.none};
+		CursorColumn=	{fg=colors.none,			bg=colors.black3, gui=colors.none};
+		PmenuSel=		{fg=colors.none,			bg=colors.blue4, gui=colors.none};
 
 		--Screen tooling
-		ColorColumn=	{fg=colors.none,			bg=colors.black,	gui=colors.none};
-		CursorColumn=	{fg=colors.none,			bg=colors.offblack, gui=colors.none};
-		Cursor=			{fg=colors.shadow,			bg=colors.white,	gui=colors.none};
+		Cursor=			{fg=colors.black3,			bg=colors.white,	gui=colors.none};
 		CursorLine=		{							bg=colors.offblack					};
-		CursorLineNr=	{fg=colors.slateblue,							gui=styles.bold};
 		IncSearch=		{fg=colors.yellow,			bg=colors.black,	gui=styles.r};
-		LineNr=			{fg=colors.blue5,											};
-		MatchParen=		{fg=colors.red,				bg=colors.none,		gui=styles.underline};
+		MatchParen=		{fg=colors.orange,				bg=colors.none,		gui=styles.underline};
 		Pmenu=			{fg=colors.none,			bg=colors.none,		gui=colors.none};
-		PmenuSel=		{fg=colors.none,			bg=colors.darkgray, gui=colors.none};
 		Search=			{fg=colors.white,			bg=colors.darkblue, gui=colors.none};
-		SignColumn=		{																};
-		StatusLine=		{fg=colors.white,			bg=colors.gray,		gui=styles.bold};
-		StatusLineNC=	{fg=colors.white,			bg=colors.none,		gui=colors.none};
-		TabLine=		{																};
-		Visual=			{fg=colors.none,			bg=colors.darkgray, gui=colors.none};
-		WinSeparator=	{fg=colors.blue,			bg=colors.none,		gui=colors.none};
 
 		-- Diff Stuff
-		DiffAdd=		{fg=colors.white,			bg=colors.green,	gui=styles.bold};
-		DiffChange=		{fg=colors.white,			bg=colors.cobalt,	gui=colors.none};
-		DiffDelete=		{fg=colors.darkred,			bg=colors.none,		gui=colors.none};
-		DiffText=		{fg=colors.white,			bg=colors.darkblue, gui=styles.bold};
+		DiffAdd=		{fg=colors.white,			bg=colors.green4,	gui=styles.none};
+		DiffChange=		{fg=colors.white,			bg=colors.indigo4,	gui=colors.none};
+		DiffDelete=		{fg=colors.orange,			bg=colors.none,		gui=colors.none};
+		DiffText=		{fg=colors.white,			bg=colors.none,		gui=styles.none};
 
 		-- Messages
 		ErrorMsg=		{fg=colors.white,			bg=colors.red,		gui=colors.none};
-		WarningMsg=		{fg=colors.white,			bg=colors.brown,		gui=colors.none};
+		WarningMsg=		{fg=colors.white,			bg=colors.brown,	gui=colors.none};
 
 		-- Text types
-		Boolean=		{fg=colors.lightpurple,		bg=colors.none,		gui=colors.none};
-		Character=		{fg=colors.lightpurple,		bg=colors.none,		gui=colors.none};
-		Comment=		{fg=colors.darkgreen,		bg=colors.none,		gui=colors.none};
-		Conditional=	{fg=colors.red,				bg=colors.none,		gui=styles.i};
-		Constant=		{fg=colors.lightpurple,		bg=colors.none,		gui=colors.none};
-		Define=			{fg=colors.red,				bg=colors.none,		gui=colors.none};
-		Directory=		{fg=colors.violet,			bg=colors.none,		gui=colors.none};
-		Float=			{fg=colors.lightpurple,		bg=colors.none,		gui=colors.none};
-		Folded=			{fg=colors.gray,			bg=colors.shadow,	gui=colors.none};
-		Function=		{fg=colors.lightmagenta,	bg=colors.none,		gui=colors.none};
-		Identifier=		{fg=colors.lightcyan,		bg=colors.none,		gui=styles.none};
-		Keyword=		{fg=colors.specialcyan,		bg=colors.none,		gui=styles.bold};
-		Label=			{fg=colors.lightyellow,		bg=colors.none,		gui=colors.none};
+		Constant=		{fg=colors.blue2,		bg=colors.none,		gui=colors.none};
+		Boolean=		{link="Constant"};
+		Character=		{link="Constant"};
+		Float=			{link="Constant"};
+		Number=			{link="Constant"};
+
+		String=			{fg=colors.green1,		bg=colors.none,		gui=colors.none};
+		Function=		{fg=colors.blue1,	bg=colors.none,		gui=colors.none};
+		Comment=		{fg=colors.green5,		bg=colors.none,		gui=colors.none};
 		NonText=		{fg=colors.none,			bg=colors.none,		gui=colors.none};
-		Normal=			{fg=colors.lightgray,		bg=colors.none,		gui=colors.none};
-		Number=			{fg=colors.lightpurple,		bg=colors.none,		gui=colors.none};
-		Operator=		{fg=colors.lightmagenta,	bg=colors.none,		gui=colors.none};
-		PreProc=		{fg=colors.violet,			bg=colors.none,		gui=colors.none};
-		SpecialComment=	{fg=colors.gray,			bg=colors.none,		gui=colors.none};
-		Special=		{fg=colors.specialcyan,		bg=colors.none,		gui=colors.none};
+		Normal=			{fg=colors.indigo1,		bg=colors.background,		gui=colors.none};
+
+		Conditional=	{fg=colors.indigo5,				bg=colors.none,		gui=styles.i};
+		Define=			{fg=colors.indigo5,				bg=colors.none,		gui=colors.none};
+		Directory=		{fg=colors.violet,			bg=colors.none,		gui=colors.none};
+		Folded=			{fg=colors.gray,			bg=colors.shadow,	gui=colors.none};
+		Identifier=		{fg=colors.lightcyan,		bg=colors.none,		gui=styles.none};
+		Keyword=		{fg=colors.green4,		bg=colors.none,		gui=styles.bold};
+		Label=			{fg=colors.lightyellow,		bg=colors.none,		gui=colors.none};
+		Operator=		{fg=colors.orange,	bg=colors.none,		gui=colors.none};
+		PreProc=		{fg=colors.blue5,			bg=colors.none,		gui=colors.none};
+		SpecialComment=	{fg=colors.black1,			bg=colors.none,		gui=colors.none};
+		Special=		{fg=colors.blue3,		bg=colors.none,		gui=colors.none};
 		SpecialKey=		{fg=colors.darkgray,		bg=colors.offblack, gui=colors.none};
 		Statement=		{fg=colors.lightblue,		bg=colors.none,		gui=colors.none};
 		StorageClass=	{fg=colors.lightcyan,		bg=colors.none,		gui=styles.none};
-		String=			{fg=colors.seagreen,		bg=colors.none,		gui=colors.none};
-		Tag=			{fg=colors.red,				bg=colors.none,		gui=colors.none};
-		Title=			{fg=colors.white,			bg=colors.none,		gui=styles.bold};
+		Tag=			{fg=colors.indigo5,				bg=colors.none,		gui=colors.none};
+		Title=			{fg=colors.white1,			bg=colors.none,		gui=styles.bold};
 		Todo=			{fg=colors.black,			bg=colors.darkgreen,gui=colors.none};
- 		Type=			{fg=colors.slateblue,		bg=colors.none,		gui=styles.b};
+ 		Type=			{fg=colors.blue2,		bg=colors.none,		gui=styles.b};
 		Underlined=		{fg=colors.none,			bg=colors.none,		gui=styles.underline};
-		Whitespace=		{fg=colors.darkgray											};
+		Whitespace=		{fg=colors.black2};
 
 		-- Diagnostics
 		DiagnosticError=			{fg=colors.red,		bg=colors.offblack};
 		DiagnosticHint=				{fg=colors.cyan,	bg=colors.offblack};
 		DiagnosticInfo=				{fg=colors.white,	bg=colors.offblack};
-		DiagnosticWarn=				{fg=colors.brown,	bg=colors.offblack};
-		DiagnosticUnnecessary=		{fg=colors.darkgray,					gui=styles.u};
+		DiagnosticWarn=				{fg=colors.orange,	bg=colors.offblack};
+		DiagnosticUnnecessary=		{fg=colors.black2,					gui=styles.u};
 
 		DiagnosticSignError =		{fg=colors.red,		bg=colors.none};
 		DiagnosticSignHint =		{fg=colors.cyan,	bg=colors.none};
@@ -117,17 +115,19 @@ function M.load_colors()
 		-- Supported Plugins:
 		--
 		-- GitGutter
-		GitGutterAdd=		{fg=colors.darkgreen};
-		GitGutterChange=	{fg=colors.cobalt};
-		GitGutterDelete=	{fg=colors.darkred};
-
+		GitGutterAdd=		{fg=colors.green5};
+		GitGutterChange=	{fg=colors.blue4};
+		GitGutterDelete=	{link="DiffDelete"};
+		GitSignsCurrentLineBlame= {link="SpecialComment"};
 		-- Fugitive
+		fugitiveStagedSection = {fg=colors.indigo2};
+		fugitiveUnstagedSection = {fg=colors.indigo4};
 		diffOldFile= {fg=colors.darkred};
 		diffNewFile= {fg=colors.darkgreen};
 
-		diffAdded=		{fg=colors.seagreen };
-		diffRemoved=	{fg=colors.darkred };
-		gitDiff=		{fg=colors.grey };
+		diffAdded=		{link="DiffAdd"};
+		diffRemoved=	{fg=colors.orange};
+		gitDiff=		{fg=colors.white4};
 
 		-- LangStuff, Push this to other places probably
 		RustInlay = {fg=colors.gray};
@@ -135,22 +135,27 @@ function M.load_colors()
 
 	}
 	-- Treesitter stuff
-	syntax["@attribute.zig"] = {fg=colors.violet};
-	syntax["@variable"] = {fg=colors.lightcyan, gui=styles.none};
-	syntax["@type.qualifier"] = {fg=colors.seablue};
-	syntax["@keyword.return"] = {fg=colors.rose};
-	syntax["@keyword.repeat"] = {fg=colors.rose};
-	syntax["@keyword.conditional"] = {fg=colors.rose};
+	syntax["@variable"] = {fg=colors.indigo2, gui=styles.none};
+
+
+	syntax["@attribute.zig"] = {fg=colors.blue3};
+	syntax["@type.qualifier"] = {fg=colors.blue1};
+	syntax["@keyword"] = {link = "Keyword"};
+	syntax["@keyword.return"] = {link = "Keyword", gui=styles.i};
+	syntax["@keyword.repeat"] = {link = "Keyword"};
+	syntax["@keyword.conditional"] = {link = "Keyword"};
+	syntax["@keyword.exception.zig"] = {link = "Operator"};
+	syntax["@lsp.type.comment.documentation"] = {fg=colors.back1};
 
 	-- Semantic highlights
-	syntax["@lsp.type.operator.zig"] = {fg=colors.lightmagenta};
-	syntax["@lsp.type.namespace"] = {fg=colors.violet};
-	syntax["@lsp.type.property"] = {fg=colors.lightbrown};
-	syntax["@lsp.type.parameter"] = {fg=colors.blue};
-	syntax["@lsp.type.variable"] = {fg=colors.celeste, gui=styles.none};
-	syntax["@lsp.type.label"] = {fg=colors.red};
+	syntax["@lsp.type.operator.zig"] = {link="Operator"};
+	syntax["@lsp.type.namespace"] = {fg=colors.blue4};
+	syntax["@lsp.type.property"] = {fg=colors.purple};
+	syntax["@lsp.type.parameter"] = {fg=colors.green2};
+	syntax["@lsp.type.variable"] = {fg=colors.indigo3, gui=styles.none};
+	syntax["@lsp.type.label"] = {fg=colors.white3};
 	syntax["@lsp.mod.declaration"] = {gui=styles.i};
-	syntax["@lsp.type.errorTag"] = {fg=colors.slateblue,		bg=colors.none,		gui=styles.b}
+	syntax["@lsp.type.errorTag"] = {link="Type",	gui=styles.b}
 
 	return syntax
 end
@@ -161,7 +166,7 @@ function M.colorscheme()
 		api.nvim_command('syntax reset')
 	end
 	-- This is a dark background
-	vim.o.background = 'dark'
+	vim.o.background = 'light'
 	vim.o.termguicolors = true
 	vim.g.colors_name = 'monocle'
 	local syntax = M.load_colors()
--- lua/palette.lua
+++ lua/palette.lua
@@ -1,23 +1,35 @@
 local M = {
-	blue1 = "#4b4bac";
-	blue2 = "#2d2da3";
-	blue3 = "#1a1a86";
-	blue4 = "#0f0f6e";
-	blue5 = "#070750";
 
-	purple1 = "#6d41a7";
-	purple2 = "#58239e";
-	purple3 = "#421182";
-	purple4 = "#32086a";
-	purple5 = "#24044e";
+	indigo1 = "#97a2d8";
+	indigo2 = "#4f74b3";
+	indigo3 = "#4d62a3";
+	indigo4 = "#515f7c";
+	indigo5 = "#3d464b";
 
-	teal1 = "#3c739e";
-	teal2 = "#1e5296";
-	teal3 = "#0e4c7b";
-	teal4 = "#063c65";
-	teal5 = "#032b4a";
+	blue1 = "#8bcfdc";
+	blue2 = "#44a5c6";
+	blue3 = "#1094ba";
+	blue4 = "#155979";
+	blue5 = "#00425d";
 
-	black = '#222222';
+	green1 = "#618887";
+	green2 = "#4e747d";
+	green4 = "#425542";
+	green5 = "#014a41";
+
+	purple = "#644b4f";
+
+	orange = "#b08f59";
+	yellow = "#beba56";
+
+	white1 = "#ede3e2";
+	white2 = "#9ba1b9";
+	white3 = "#80767e";
+	white4 = "#6c656c";
+
+	black1 = '#555555';
+	black2 = "#222222";
+	black3 = "#191722";
 
 	none = "NONE";
 }
